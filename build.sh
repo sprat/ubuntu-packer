235797a1ad48d2bedb7303a56d6aaa867a42b47b
Sylvain Prat <sylvain.prat@gmail.com>
2019-01-22 20:26:30 +0100

    Handle daily build easily

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Usage: $0 name"
     echo "where name: xubuntu | ..."
     exit 1
 fi
 
-packer build -var-file packer/$1.json packer/template.json
+UBUNTU_ISO_FILENAME="bionic-server-amd64.iso"
+UBUNTU_ISO_BASE_URL="http://cdimage.ubuntu.com/ubuntu-server/bionic/daily/$(date -u +%Y%m%d)/"
+
+# UBUNTU_ISO_FILENAME="ubuntu-18.04.1-server-amd64.iso"
+# UBUNTU_ISO_BASE_URL="http://cdimage.ubuntu.com/releases/18.04.1/release/"
+
+packer build -var ubuntu_iso_filename=$UBUNTU_ISO_FILENAME -var ubuntu_iso_base_url=$UBUNTU_ISO_BASE_URL -var-file=packer/$1.json packer/template.json

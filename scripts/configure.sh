1f382ed4c39a53fedf8f821bb66cdee0483918af
Sylvain Prat <sylvain.prat@gmail.com>
2019-03-15 07:57:22 +0100

    Rename postinstall script to configure + hardcoded vagrant insecure public key instead of download

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -8,7 +8,7 @@ chmod 440 /etc/sudoers.d/99_vagrant
 # Add vagrant insecure SSH public key
 SSH_DIR=/home/vagrant/.ssh
 mkdir -p $SSH_DIR
-wget -q https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O $SSH_DIR/authorized_keys
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > $SSH_DIR/authorized_keys
 chown -R vagrant:vagrant $SSH_DIR
 chmod 700 $SSH_DIR
 chmod 600 $SSH_DIR/authorized_keys

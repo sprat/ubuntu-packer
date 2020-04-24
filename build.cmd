1f643a578834f353cc7d7afea767f4fe7dc92b76
Sylvain Prat <sylvain.prat@gmail.com>
2020-04-24 09:17:25 +0200

    Fix build scripts

--- build.cmd
+++ build.cmd
@@ -5,9 +5,6 @@ if "%1" == "" (
     goto :end
 )
 
-rem You may encounter 416 HTTP errors: please delete the packer_cache when it
-rem happens. It should be solved in packer version 1.3.5, see
-rem https://github.com/hashicorp/packer/issues/6443 for details
-packer build -var-file=packer/%1.json packer/template.json
+packer build -timestamp-ui -var-file=packer/%1.json packer/template.json
 
 :end
